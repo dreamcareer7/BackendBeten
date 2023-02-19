4c231168a0bee7ea03f9999884bcdd15267086fd
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-20 01:31:12 +0500

    just minor fixes

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
 
 class UserRole extends Model
 {
     use HasFactory;
+    use HasRoles;
+
 }
