fff74faf92ad0914bbc85a5c491631ae78b0b2c2
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-19 17:28:06 +0500

    Pagination added

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v2')->group(function() {
     Route::post('/sign_in', [App\Http\Controllers\API\Auth\LoginAPIController::class, 'signIn']);
-    Route::post('/sign_up', [App\Http\Controllers\API\Auth\RegisterAPIController::class, 'register']);
+
+    //we do not need it for client
+   // Route::post('/sign_up', [App\Http\Controllers\API\Auth\RegisterAPIController::class, 'register']);
 });
 
 Route::group(['middleware' => ['jwt.verify']], function () {
-
     Route::resource('/clients', App\Http\Controllers\API\ClientsAPIController::class);
     Route::resource('/services', App\Http\Controllers\API\ServiceAPIController::class);
     Route::resource('/crew', App\Http\Controllers\API\CrewAPIController::class);
