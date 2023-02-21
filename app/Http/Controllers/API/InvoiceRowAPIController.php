932112a7c50105c85a8d44696acb9e3d07bd004f
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-21 19:09:55 +0100

    Fix inheritance
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- app/Http/Controllers/API/InvoiceRowAPIController.php
+++ app/Http/Controllers/API/InvoiceRowAPIController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\API\API\Controller;
 use App\Models\InvoiceRow;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class InvoiceRowAPIController extends Controller
 {
