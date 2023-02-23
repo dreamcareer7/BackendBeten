ea031f9ceccabc21ee5ca275060cd190d6fe2524
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-23 20:52:23 +0100

    Fixed indentation
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- app/Http/Controllers/API/HospitalitiesController.php
+++ app/Http/Controllers/API/HospitalitiesController.php
@@ -11,63 +11,63 @@ use App\Http\Requests\CreateHospitalityRequest;
 
 class HospitalitiesController extends Controller
 {
-	/**
-	 * Display a listing of the hospitality.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index()
-	{
-		//
-	}
+    /**
+    * Display a listing of the hospitality.
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created hospitality in database.
-	 *
-	 * @param \App\Http\Requests\CreateHospitalityRequest $request
-	 *
-	 * @return \Illuminate\Http\JsonResponse
-	 */
-	public function store(CreateHospitalityRequest $request): JsonResponse
-	{
-		Hospitality::create($request->validated());
-		return response()->json(status: 201);
-	}
+    /**
+    * Store a newly created hospitality in database.
+    *
+    * @param \App\Http\Requests\CreateHospitalityRequest $request
+    *
+    * @return \Illuminate\Http\JsonResponse
+    */
+    public function store(CreateHospitalityRequest $request): JsonResponse
+    {
+        Hospitality::create($request->validated());
+        return response()->json(status: 201);
+    }
 
-	/**
-	 * Display the specified hospitality.
-	 *
-	 * @param \App\Models\Hospitality $hospitality
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show(Hospitality $hospitality)
-	{
-		//
-	}
+    /**
+    * Display the specified hospitality.
+    *
+    * @param \App\Models\Hospitality $hospitality
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function show(Hospitality $hospitality)
+    {
+        //
+    }
 
-	/**
-	 * Update the specified hospitality in database.
-	 *
-	 * @param \Illuminate\Http\Request $request
-	 * @param \App\Models\Hospitality $hospitality
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, Hospitality $hospitality)
-	{
-		//
-	}
+    /**
+    * Update the specified hospitality in database.
+    *
+    * @param \Illuminate\Http\Request $request
+    * @param \App\Models\Hospitality $hospitality
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function update(Request $request, Hospitality $hospitality)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified hospitality from database.
-	 *
-	 * @param \App\Models\Hospitality $hospitality
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy(Hospitality $hospitality)
-	{
-		//
-	}
+    /**
+    * Remove the specified hospitality from database.
+    *
+    * @param \App\Models\Hospitality $hospitality
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy(Hospitality $hospitality)
+    {
+        //
+    }
 }
