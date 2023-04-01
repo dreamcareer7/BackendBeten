8d204c9fb4699adc78e979ebfc1dac1901a52b32
dreamcareer7 <dreamcareer070@gmail.com>
2023-04-01 13:56:56 +0100

    Format client log datetime
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- app/Models/ClientLog.php
+++ app/Models/ClientLog.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -34,4 +35,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class ClientLog extends Model
 {
 	use HasFactory;
+
+	/**
+	 * Prepare a date for array / JSON serialization.
+	 *
+	 * @return string date formatted
+	 */
+	protected function serializeDate(DateTimeInterface $date): string
+	{
+		return $date->format('Y-m-d H:i:s');
+	}
 }
