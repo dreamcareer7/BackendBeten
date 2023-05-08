ef1072f464600bc883bf3b8a8ee1a3682c299093
dreamcareer7 <dreamcareer070@gmail.com>
2023-05-08 11:52:37 -0400

    Update eogsoft.php

--- config/eogsoft.php
+++ config/eogsoft.php
@@ -18,7 +18,7 @@ return [
 			'taqnyat'=>[
 				'bearer'=>'6ae9a69b9a06dfb7772787c1f1c4dbc4',
 				'sender'=>'ArabMotwf',
-				'sendsms'=>'sendMsg',
+				'sendsms'=>'sendMsg', // find it in helerps/functions.php
 			],
 		],
     ],
