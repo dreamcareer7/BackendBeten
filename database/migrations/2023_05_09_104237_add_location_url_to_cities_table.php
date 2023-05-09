40c920906758c75ac91ab0f673d76657f4ccd9c7
dreamcareer7 <dreamcareer070@gmail.com>
2023-05-09 10:44:18 -0400

    Create 2023_05_09_104237_add_location_url_to_cities_table.php
    adding location_url to cities table as database diagram

---
+++ database/migrations/2023_05_09_104237_add_location_url_to_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('cities', function (Blueprint $table) {
+			//
+			$table->string('location_url')->nullable();
+		 });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('cities', function (Blueprint $table) {
+			//
+		});
+	}
+};
