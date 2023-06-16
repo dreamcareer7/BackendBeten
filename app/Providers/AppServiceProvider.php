055e8dd5c5aa929e47465cd890ad46df60d39517
dreamcareer7 <dreamcareer070@gmail.com>
2023-06-16 12:20:30 -0400

    Update AppServiceProvider.php
    Added ability for hasServiceCenter to Sanctum personal API token

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,9 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+// to apply hasServiceCenter to sanctum personal API token
+use App\Models\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
 	{
 		Model::unguard();
 		Paginator::useBootstrap();
+        // to apply hasServiceCenter to Sanctum personal API token
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
 	}
 }
